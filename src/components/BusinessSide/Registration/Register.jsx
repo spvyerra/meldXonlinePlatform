3fd146e27e2474bb45ab367d501aab790ac04c5e
spvyerra <spvyerra@gmail.com>
2020-05-25 16:32:20 -0400

    Register state changes set

--- src/components/BusinessSide/Registration/Register.jsx
+++ src/components/BusinessSide/Registration/Register.jsx
@@ -4,8 +4,22 @@ import Web3 from 'web3';
 
 
 export default class BusRegister extends React.Component {
-    state = {
-        
+    constructor(props) {
+        super(props);
+    }
+
+
+    nameChange = (e) => {
+        this.setState({
+            businessName: e.target.value
+        });
+    }
+    
+    symbolChange = (e) => {
+        this.setState({
+            businessSymbol: e.target.value
+        });
+
     }
 
     render() {
@@ -16,7 +30,14 @@ export default class BusRegister extends React.Component {
                 <input type="text" placeholder="Address" />
                 <button>Add Financial Info</button>
                 <p>(Just for show does nothing right now)</p>
-                <button>Coninue</button>
+
+                <div>
+                    <h2>Contract Info</h2>
+                    <input type="text" id="busName" placeholder="Business Name" onChange={this.nameChange} />
+                    <input type="text" id="busSymbol" placeholder="Symbol 3-4 letters" onChange={this.symbolChange} />
+                </div>
+
+                <button>Continue</button>
             </div>
         );
     }
