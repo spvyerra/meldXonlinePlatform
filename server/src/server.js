5d0ef4d176e8da3948f5f1fce865690b5c760666
spvyerra <spvyerra@gmail.com>
2020-05-30 14:41:45 -0400

    Two get requests done

--- server/src/server.js
+++ server/src/server.js
@@ -1,13 +1,23 @@
 const express = require('express');
 const fs = require('fs');
 
-const overall = './server/bus.json';
+const overall = './server/assets/bus.json';
+const breakDown = './server/assets/busBreak';
 
 const app = express();
 app.use(express.json());
 
 app.get('/list', (req, res) => {
     const raw = fs.readFileSync(overall);
+    const data = JSON.parse(raw);
+
+    res.status(500).json(data);
+});
+
+app.get('/list/:id', (req, res) => {
+    const id = req.params.id;
+
+    const raw = fs.readFileSync(breakDown + `/${id}.json`);
     const data = JSON.parse(raw);
 
     res.status(500).json(data);
