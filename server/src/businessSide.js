71e1bc57605fe6a51063c4076610edb15ef2695e
spvyerra <spvyerra@gmail.com>
2020-06-02 20:11:00 -0400

    Name change

--- server/src/businessSide.js
+++ server/src/businessSide.js
@@ -11,46 +11,61 @@ let deploySecureToken = async (bus) => {
 
     let acct = admin.getAcct();
     let deployAddress;
-    
+
     // Get SecureToken contract
     fs.readFile('../../build/contracts/SecureToken.json', (err, data) => {
         if (err) throw err;
         let rawContract = JSON.parse(data);
-        
+
         let bytecode = rawContract.bytecode;
         let ownerHash = web3.utils.sha3(bus.ownerAddress);
-        
+
         let secureToken = new admin.web3.eth.Contract(rawContract.abi);
-        
+
         secureToken.deploy({
             data: bytecode,
             arguments: [bus.ownerAddress, ownerHash, bus.numShares, bus.busName, bus.symbol]
         })
-        .send({
-            from: acct.address
-        })
-        .on("transactionHash", (hash) => console.log(hash))
-        .on('receipt', (rec) => {
-            console.log(rec);
-            deployAddress = rec.address;
-        });
-        
+            .send({
+                from: acct.address
+            })
+            .on("transactionHash", (hash) => console.log(hash))
+            .on('receipt', (rec) => {
+                console.log(rec);
+                deployAddress = rec.address;
+            });
+
     });
-    
+
     admin.exit();
 
     return deployAddress;
 }
 
-let addVerified = (address) => {
-    admin.init();
+let addVerify = async () => {
 
-    let acct = admin.getAcct();
+    console.log("Hello");
 
+    let verified = await contract.methods.addVerified(rawData['ownerAdress'], rawData['name']).call({ from: acct });
+    return verified;
+}
 
+let isVerified = async () => {
+
+    let isVerified = await contract.methods.isVerified(rawData['ownerAdress']).call();
+    return isVerified;
+}
+
+let reissue = async () => {
+
+    return await contract.methods.cancelAndReissue(rawData[""], rawData[""])
 }
 
 module.exports = {
+    getAcct,
+    isVerified,
+    addVerify,
+    getBal,
     deploySecureToken,
     addVerified
 };
