725bc52c1170cba4b7482bc268653a1731a0c820
spvyerra <spvyerra@gmail.com>
2020-06-03 14:51:04 -0400

    Remove verified ready

--- server/src/businessSide.js
+++ server/src/businessSide.js
@@ -34,8 +34,9 @@ let deploySecureToken = async (bus) => {
     return deployAddress;
 }
 
-let isVerified = (address) => {
+let isVerifiedLocal = (address) => {
     let data = JSON.parse(fs.readFileSync(verifiedPath));
+    data = data.filter((obj) => obj);
 
     for (i in data) {
         if (data[i].address == address) {
@@ -49,6 +50,20 @@ let isVerified = (address) => {
 let addVerify = async (addressObj) => {
     admin.init();
     let acct = admin.getAcct();
+
+    if (!isVerifiedLocal(addressObj.userAddress)) {
+        fs.readFile(verifiedPath, (err, data) => {
+            data = JSON.parse(data);
+            data.push({
+                "id": data.length,
+                "address": addressObj.userAddress
+            });
+
+            data = data.filter((obj) => obj);
+            fs.writeFileSync(verifiedPath, JSON.stringify(data, null, 4));
+            console.log("File written");
+        });
+    }
 
     secureToken.options.address = addressObj.contAddress;
     let userHash = admin.web3.utils.sha3(addressObj.userAddress);
@@ -71,21 +86,47 @@ let addVerify = async (addressObj) => {
         });
 
     admin.exit();
-
-    fs.readFile(verifiedPath, (err, data) => {
-        data = JSON.parse(data);
-        data.push({
-            "id": data.length,
-            "address": addressObj.userAddress
-        });
-
-        fs.writeFileSync(verifiedPath, JSON.stringify(data, null, 4));
-        console.log("File written");
-    });
 }
 
 let removeVerified = async (addressObj) => {
+    admin.init();
+    let acct = admin.getAcct();
+    secureToken.options.address = addressObj.contAddress;
 
+    if (isVerifiedLocal(addressObj.userAddress)) {
+        fs.readFile(verifiedPath, (err, data) => {
+            data = JSON.parse(data);
+
+            for (i in data) {
+                if (data[i].address == addressObj.userAddress) {
+                    delete data[i];
+                }
+            }
+
+            data = data.filter((obj) => obj);
+            fs.writeFileSync(verifiedPath, JSON.stringify(data, null, 4));
+            console.log("File written");
+        });
+    }
+
+    await secureToken.methods
+        .removeVerified(addressObj.userAddress)
+        .send({ from: acct.address })
+        .on("transactionHash", (hash) => console.log(hash))
+
+        .on("receipt", (rec) => {
+            console.log("Tx completed");
+            console.log(rec);
+        })
+
+        .on("error", (err, rec) => {
+            console.log("error occured");
+            console.log(rec);
+            console.log(err);
+        });
+
+
+    admin.exit();
 }
 
 let reissue = async () => {
@@ -93,10 +134,10 @@ let reissue = async () => {
     return await contract.methods.cancelAndReissue(rawData[""], rawData[""]);
 }
 
-
 module.exports = {
     addVerify,
     deploySecureToken,
-    isVerified,
-    reissue
+    isVerifiedLocal,
+    reissue,
+    removeVerified
 };
