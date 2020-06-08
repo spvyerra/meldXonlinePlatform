7cc31d0d671fbeab36e42ead270ada62b1964e49
spvyerra <spvyerra@gmail.com>
2020-06-08 17:54:45 -0400

    adding and removing shares from protfolio ready

--- server/src/portfolio.js
+++ server/src/portfolio.js
@@ -1,25 +1,54 @@
 const fs = require('fs');
 const verify = require('./verfied');
 
-const protfolioPath = "./server/assets/portfolio";
+const portfolioPath = "./server/assets/portfolio";
 
 let getPortfolio = (address) => {
     let addressObj = verify.getVerified(address);
 
-    const portfolio = JSON.parse(fs.readFileSync(protfolioPath + `/${addressObj.id}.json`));
+    const portfolio = JSON.parse(fs.readFileSync(portfolioPath + `/${addressObj.id}.json`));
 
     return portfolio;
 }
 
 let addShares = (address, contractID, numShares) => {
-    let portfolio = getPortfolio(address);
+    let port = getPortfolio(address);
 
-    
+    let thing = port.shares.find((item) => item.id == contractID);
 
+    if (thing != null) {
+        console.log(thing);
+        thing.owned += numShares;
+    } else {
+        port.shares.push({
+            "id": contractID,
+            "owned": numShares
+        });
+    }
+
+    console.log(port);
+
+    fs.writeFile(portfolioPath + `/${port.id}.json`, JSON.stringify(port, null, 4),
+        (err) => { if (err) console.log(err) });
 }
 
 let removeShares = (address, contractID, numShares) => {
-    
+    let port = getPortfolio(address);
+
+    let ind = port.shares.findIndex((item) => item.id == contractID);
+
+    console.log(ind);
+
+    if (port.shares[ind].owned == numShares)
+        delete port.shares[ind];
+    else
+        port.shares[ind].owned -= numShares;
+
+    console.log(port);
+
+    port.shares = port.shares.filter((obj) => obj);
+    fs.writeFile(portfolioPath + `/${port.id}.json`, JSON.stringify(port, null, 4),
+        (err) => console.log(err));
 }
 
 module.exports = {
